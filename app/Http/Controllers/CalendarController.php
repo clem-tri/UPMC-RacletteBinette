458680522f95a60b9938357c6c51e924d5e3ed3f
clemtri <ctritta@outlook.fr>
2018-06-23 23:15:03 +0200

    add calendar controller+view

---
+++ app/Http/Controllers/CalendarController.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Clement
+ * Date: 23/06/2018
+ * Time: 19:09
+ */
+
+namespace App\Http\Controllers;
+
+
+use AvoRed\Framework\Models\Database\Product;
+use MaddHatter\LaravelFullcalendar\Facades\Calendar;
+
+class CalendarController extends Calendar
+{
+
+    public function index(){
+
+        $events = [];
+
+        $events[] = Calendar::event(
+            'Event One', //event title
+            false, //full day event?
+            '2015-02-11T0800', //start time (you can also use Carbon instead of DateTime)
+            '2015-02-12T0800', //end time (you can also use Carbon instead of DateTime)
+            0 //optionally, you can specify an event ID
+        );
+
+        $events[] = Calendar::event(
+            "Période d'utilisation", //event title
+            true, //full day event?
+            new \DateTime('2018-06-14'), //start time (you can also use Carbon instead of DateTime)
+            new \DateTime('2018-06-18'), //end time (you can also use Carbon instead of DateTime)
+            'stringEventId' //optionally, you can specify an event ID
+        );
+
+        $calendar = Calendar::addEvents($events)->setOptions(['lang' => 'fr']);
+
+
+        return view('calendar.view')->with('calendar', $calendar);
+    }
+
+}
---
+++ themes/TSDC/default/views/calendar/view.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+
+@section('meta_title','Calendrier d\'utilisation')
+@section('meta_description','Calendrier d\'utilisation')
+
+@section('js')
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/lang-all.js"></script>
+@endsection
+
+@section('css')
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.css"/>
+@endsection
+
+
+@section('content')
+{!! $calendar->calendar() !!}
+{!! $calendar->script() !!}
+@endsection
