edd7cc3ffad24c219e1e5115dc92728240be5df6
TiagoC22 <leportuguesinho@hotmail.fr>
2018-06-27 23:50:53 +0200

    Style page Supression du compte et changement d'image profil utilisateur

--- themes/TSDC/default/views/user/my-account/delete.blade.php
+++ themes/TSDC/default/views/user/my-account/delete.blade.php
@@ -5,37 +5,40 @@
 
 
 @section('content')
+    <div class="container">
     <div class="row profile">
         <div class="col-md-3">
             @include('user.my-account.sidebar')
         </div>
         <div class="col-md-9">
 
-            <div class="card">
-                <div class="card-header">
-                   Supprimer mon compte et mes données personnelles
-                </div>
-                <div class="card-body">
+            <div class="card" style="background-color:#fff; border:2px solid #68B42F; border-radius:12px;">
+                <div class="card-body" style="background-color:#68B42F; border-bottom-left-radius:0px; border-bottom-right-radius:0px;">
+                    <div class="card-header" style="background-color:#68B42F;"><span class="title_auth"><p style="color:white">Supprimer mon compte et mes donnees personnelles</p></span></div></div>
 
-                    <p>
-                        Vous pouvez supprimer votre compte.
-                        Sachez toutefois qu'il ne sera PAS possible de le réactiver par la suite.
-                        Il s'agit d'une action définitive.
-                    </p>
+
+
+                <p style="color:#68B42F; margin-right:10px; margin-left:10px; margin-top:10px;">
+                    Vous pouvez supprimer votre compte.<br/>
+                    Sachez toutefois qu'il ne sera PAS possible de le réactiver par la suite.
+                    Il s'agit d'une action définitive.
+                </p>
 
                     <form action="{{ route('my-account.delete.post') }}" method="post">
                         {{ csrf_field() }}
 
                         <div class="form-group">
-                            <button class="btn btn-danger" type="submit">Supprimer votre compte</button>
+                            <button type="submit" class="btn btn-danger" style="color:white !important; background-image:none; display: block; margin : auto; font-size:10pt;">
+                               Supprimer mon compte
+                            </button>
                         </div>
 
                     </form>
 
                 </div>
             </div>
-
         </div>
+    </div>
     </div>
 
 @endsection
--- themes/TSDC/default/views/user/my-account/upload-image.blade.php
+++ themes/TSDC/default/views/user/my-account/upload-image.blade.php
@@ -4,23 +4,24 @@
 @section('meta_description','Upload My Account E commerce')
 
 @section('content')
+    <div class="container">
     <div class="row profile">
         <div class="col-3">
             @include('user.my-account.sidebar')
         </div>
         <div class="col-9">
 
-            <div class="card">
-                <div class="card-header">
-                    Image de profil
-                </div>
-                <div class="card-body">
-                    <form action="{{ route('my-account.upload-image.post') }}" method="post" enctype="multipart/form-data">
+            <div class="card" style="background-color:#fff; border:2px solid #68B42F; border-radius:12px;">
+                <div class="card-body" style="background-color:#68B42F; border-bottom-left-radius:0px; border-bottom-right-radius:0px;">
+                    <div class="card-header" style="background-color:#68B42F;"><span class="title_auth"><p style="color:white">Image de profil</p></span> </div> </div>
+
+
+                <form action="{{ route('my-account.upload-image.post') }}" method="post" enctype="multipart/form-data">
 
                         {{ csrf_field() }}
-                        <div class="form-group">
+                        <div class="form-group"  style="padding-top:20px;">
                             <label for="profile_image">Image de profil</label>
-                            <input type="file"
+                            <input type="file" style="border-radius:0px; border-left:none; border-right:none;"
                                    @if($errors->has('profile_image'))
                                    class="is-invalid form-control"
                                    @else
@@ -35,14 +36,15 @@
                             @endif
                         </div>
 
-                        <div class="form-group">
-                            <button class="btn btn-primary" type="submit">Changer l'image</button>
-                        </div>
-                    </form>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary" style="background-color:#68B42F !important; color:white !important; background-image:none; display: block; margin : auto; font-size:10pt;">
+                            Changer l'image
+                        </button>
+                    </div>
 
+                    </form>
                 </div>
             </div>
-
         </div>
     </div>
 
