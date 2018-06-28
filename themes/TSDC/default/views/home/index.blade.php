54631614ac447b00f605625effde65d9aab8d427
TiagoC22 <leportuguesinho@hotmail.fr>
2018-06-28 10:15:37 +0200

    Boutons page d'accueil

--- themes/TSDC/default/views/home/index.blade.php
+++ themes/TSDC/default/views/home/index.blade.php
@@ -18,9 +18,9 @@
     <div class="row" style="width:100%;">
         <div class="col-md-11 col-centered produitEnUne col-md-offset-1" style="background-size:cover; min-height:400px;">
             <nav aria-label="navigation">
-                <ul class="pager" style="position:absolute; bottom:0; padding-bottom:150px; padding-right:50px; right:0;">
-                    <li><a href="#" title="Suivant">Découvrir</a></li>
-                </ul>
+                <div class="pager" style="position:absolute; bottom:0; padding-bottom:150px; padding-right:50px; right:0;">
+                    <a class="btn btn-primary" style="background-color:#68B42F; border-color:white;" href="#" role="button">Découvrir</a>
+                </div>
             </nav>
         </div>
     </div>
@@ -32,48 +32,48 @@
             <h2 style="font-family:'Big John' !important; color:white; text-align:center; font-size:20pt; padding-top:170px;">Sentez la nature vous effleurer</h2>
             <h3 style="font-family:'Slim Joe' !important; color:white; text-align:center; font-size:16pt; ">Plante d'exterieur</h3>
             <nav aria-label="navigation">
-                <ul class="pager" style="position:absolute; bottom:0; padding-bottom:150px; padding-right:50px; right:0;">
-                    <li><a href="#" title="Suivant">Découvrir</a></li>
-                </ul>
+                <div class="pager" style="position:absolute; bottom:0; padding-bottom:150px; padding-right:50px; right:0;">
+                    <a class="btn btn-primary" style="background-color:#68B42F; border-color:white;" href="#" role="button">Découvrir</a>
+                </div>
             </nav>
         </div>
         <div class="col-md-3 col-centered forumImage"  style="background-size:cover; min-height:450px;">
-            <h2 style="font-family:'Big John' !important; color:white; text-align:center; font-size:20pt; padding-top:310px;">Visitez notre forum</h2>
+            <h2 style="font-family:'Big John' !important; color:white; text-align:center; font-size:20pt; padding-top:250px;">Visitez notre forum</h2>
             <nav aria-label="navigation">
-                <ul class="pager" style="position:relative; bottom:0; margin-right:auto; margin-left:auto;">
-                    <li><a href="#" title="Suivant">Visiter</a></li>
-                </ul>
+                <div class="pager" style="position:relative; bottom:0; padding-top:50px;">
+                    <a class="btn btn-primary" style="background-color:white; font-weight:bold; color:#68B42F; border-color:white; position:relative; display:block; width:100px; margin-right:auto; margin-left:auto;" href="#" role="button">Visiter</a>
+                </div>
             </nav>
         </div>
     </div>
     <br/>  <br/>  <br/>
 
     <div class="row" style="width:100%;">
-        <div class="col-md-3 col-centered produit1 col-md-offset-1"  style="max-width : 100%;min-height: 300px; display: block;">
+        <div class="col-md-3 col-centered produit1 col-md-offset-1"  style="max-width : 100%;min-height: 250px; display: block;">
             <div class="circle"><h3 style="font-size:15pt; font-family: 'Big John'; padding-top:20px;">54,35e</h3><h3 style="font-size:7pt; font-family: 'Slim Joe';">HERELUDIT BALCON</h3>
             </div>
             <nav aria-label="navigation">
-                <ul class="pager" style="position:absolute; bottom:0; right:0; padding-right:20px; padding-bottom:20px;">
-                    <li><a href="#" title="Voir">Voir le produit</a></li>
-                </ul>
+                <div class="pager" style="position:absolute; bottom:0; padding-bottom:10px; padding-right:50px; right:0;">
+                    <a class="btn btn-primary" style="background-color:#68B42F; border-color:white;" href="#" role="button">Voir le produit</a>
+                </div>
             </nav>
         </div>
-        <div class="col-md-3 col-centered produit2 col-md-offset-1"  style="max-width : 100%;min-height: 300px; display: block;">
+        <div class="col-md-3 col-centered produit2 col-md-offset-1"  style="max-width : 100%;min-height: 250px; display: block;">
             <div class="circle"><h3 style="font-size:15pt; font-family: 'Big John'; padding-top:20px;">20e</h3><h3 style="font-size:7pt; font-family: 'Slim Joe';">BROUETTE JARDINAGE</h3>
             </div>
             <nav aria-label="navigation">
-                <ul class="pager" style="position:absolute; bottom:0; right:0; padding-right:20px; padding-bottom:20px;">
-                    <li><a href="#" title="Voir">Voir le produit</a></li>
-                </ul>
+                <div class="pager" style="position:absolute; bottom:0; padding-bottom:10px; padding-right:50px; right:0;">
+                    <a class="btn btn-primary" style="background-color:#68B42F; border-color:white;" href="#" role="button">Voir le produit</a>
+                </div>
             </nav>
         </div>
-        <div class="col-md-3 col-centered produit3 col-md-offset-1"  style="max-width : 100%;min-height: 300px; display: block;">
+        <div class="col-md-3 col-centered produit3 col-md-offset-1"  style="max-width:100%; min-height: 250px; display: block;">
             <div class="circle"><h3 style="font-size:15pt; font-family: 'Big John'; padding-top:20px;">128,99e</h3><h3 style="font-size:7pt; font-family: 'Slim Joe';">TONDEUSE CLUB CADET</h3>
             </div>
             <nav aria-label="navigation">
-                <ul class="pager" style="position:absolute; bottom:0; right:0; padding-right:20px; padding-bottom:20px;">
-                    <li><a href="#" title="Voir">Voir le produit</a></li>
-                </ul>
+                <div class="pager" style="position:absolute; bottom:0; padding-bottom:10px; padding-right:50px; right:0;">
+                    <a class="btn btn-primary" style="background-color:#68B42F; border-color:white;" href="#" role="button">Voir le produit</a>
+                </div>
             </nav>
         </div>
     </div>
